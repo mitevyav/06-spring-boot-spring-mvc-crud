c3b24887e6d547d433db3ecb1fd40e5543c084e8
yavor <jm87585@gmail.com>
2023-07-29 16:50:45 +0300

    Implement a page with CRUD operations for Employee

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,81 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model theModel) {
+
+        List<Employee> employees = employeeService.findAll();
+
+        // add to the spring model
+        theModel.addAttribute("employees", employees);
+
+        return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        Employee employee = new Employee();
+
+        model.addAttribute("employee", employee);
+
+        return "employees/employee-form";
+    }
+
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(
+            @RequestParam("employeeId") int employeeId,
+            Model model
+    ) {
+        Employee employee = employeeService.findById(employeeId);
+
+        model.addAttribute("employee", employee);
+
+        return "employees/employee-form";
+    }
+
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+
+        employeeService.save(employee);
+
+        return "redirect:/employees/list";
+    }
+
+    @GetMapping("/delete")
+    public String deleteEmployee(@RequestParam("employeeId") int employeeId) {
+
+        employeeService.deleteById(employeeId);
+
+        return "redirect:/employees/list";
+    }
+}
+
+
+
+
+
+
+
+
+
